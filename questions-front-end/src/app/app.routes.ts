82f39c93478835bd5d3fde3abec4df51d9479f37
intotito <intotito@gmail.com>
2024-06-23 06:48:01 +0100

    Reading XML files from file system

--- questions-front-end/src/app/app.routes.ts
+++ questions-front-end/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // add lazy loaded routes for welcome as home page
+    { path:  '', component:  WelcomeComponent}
+
+];
---
+++ questions-front-end/src/app/components/welcome/welcome.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { readFileSync } from 'fs';
+
+@Component({
+  selector: 'app-welcome',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.css'
+})
+export class WelcomeComponent {
+  //imagePath = 'assets/atu.png';
+  // valid path to image at assets/images/atu-brand.png
+  imagePath = 'assets/images/atu-brand.png';
+  msg = 'No File Selected';
+  xml = '';
+
+  constructor() { }
+
+  async onFileSelected(event: any) {
+    const file:File = event?.target?.files[0];
+    if (file) {
+      this.msg = file.webkitRelativePath || file.name;
+    } else {
+      this.msg = 'No File Selected';
+    }
+    this.xml = await file.text();
+    console.log(this.xml);
+  }
+}
